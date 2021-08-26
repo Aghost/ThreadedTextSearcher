b4d6fc47e37a06bdbf6a70243f7e88934fcb0ac8
Aghost <youri_van_sorgen2@example.com>
2021-08-26 21:02:30 +0200

    init commit

---
+++ TTSearch.App/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using static System.Console;
+using System.Threading;
+using System.Threading.Tasks;
+using TTSearch.Business;
+
+namespace TTSearch.App
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            if (args.Length > 0) {
+                var fr = new FileReader(@"../../0_LIBRARY/", args);
+
+                fr.Testing();
+                fr.FindInFiles();
+            }
+        }
+    }
+}
---
+++ TTSearch.Business/FileReader.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using static System.Console;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace TTSearch.Business
+{
+    public class FileReader
+    {
+        private string _FilePath { get; set; }
+        private string[] _KeyWords { get; set; }
+
+        private int filesRead;
+        private int totalLinesRead;
+
+        public FileReader(string FilePath, string[] KeyWords) {
+            this._FilePath = FilePath;
+            this._KeyWords = KeyWords;
+        }
+
+        public void Testing() {
+            WriteLine($"{_FilePath}, {String.Join(" ", _KeyWords)}");
+        }
+
+        public void PrintResults() {
+        }
+
+        public void FindInFiles() {
+            string[] fileSystemEntries = Directory.GetFiles(_FilePath, "*.*", SearchOption.AllDirectories);
+            List<string> res = new List<string>();
+
+            Parallel.ForEach(fileSystemEntries, i => { ReadFile(i, res); });
+
+            WriteLine("Read {0} lines in {1} file(s).", totalLinesRead, filesRead);
+            WriteLine("Press a key to see the results:");
+            ReadKey();
+
+            res.ForEach(r => WriteLine(r));
+        }
+
+        private void ReadFile(string filePath, List<string> results) {
+            string[] fileDataArray = File.ReadAllLines(filePath);
+            int linesRead = 0;
+
+            //results.Add($"file {filePath} threadID: {Thread.CurrentThread.ManagedThreadId}");
+
+            Parallel.ForEach(fileDataArray, line => {
+                if (line.Contains(_KeyWords[0])) { results.Add(
+                    $"\t>({filePath}) contains '{_KeyWords[0]}' at line {linesRead}:\n\"{line}\" (thread: {Thread.CurrentThread.ManagedThreadId})\n"
+                );}
+
+                linesRead++;
+            }); 
+
+            totalLinesRead += linesRead;
+            filesRead++;
+        }
+    }
+}
